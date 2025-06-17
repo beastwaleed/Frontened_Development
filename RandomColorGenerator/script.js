5e51351ee0504b8adb9c24accf49040f00f02e56
Waleed Afzal <144891433+beastwaleed@users.noreply.github.com>
2025-06-17 19:08:24 +0500

    Add files via upload

---
+++ RandomColorGenerator/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const container = document.querySelector(".container");
+
+    for (let i = 0; i < 16; i++) {
+        const colorContainerDiv = document.createElement("div");
+        colorContainerDiv.classList.add("color-container");
+        container.appendChild(colorContainerDiv);
+    }
+
+    const colorContainersALL = document.querySelectorAll(".color-container");
+
+    generateColors();
+
+    function generateColors() {
+        colorContainersALL.forEach((colorContainerDiv) => {
+            colorContainerDiv.style.backgroundColor = randomColors();
+            colorContainerDiv.innerText = randomColors();
+
+        });
+
+    }
+
+    function randomColors() {
+        const chars = "0123456789abcdf";
+        const colorLength = 6;
+        let ColorCode = "#";
+
+        for (let i = 0; i < colorLength; i++) {
+            let randomNum = Math.floor(Math.random() * chars.length);
+            ColorCode += chars[randomNum]
+        }
+        return ColorCode;
+    }
+
+});
